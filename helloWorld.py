0bbaf9d3015ac756746bed9c5f3afd105b78a388
sonyaqaderi1 <sq350@bath.ac.uk>
2024-03-09 15:14:33 +0000

    Added pet information and printed it

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,17 @@
+# This script gathers information on the user
+
 name = input("Enter your name: ")
 surname = input("Enter your surname: ")
 age = int(input("Enter your age: "))
-print(f"Hi {name} {surname}! You're {age} years old!")
+email = input("Enter your email: ")
+
+# Print out the information
+print(f"Hi {name} {surname}! You're {age} years old and have the email: {email}.")
+
+# Ask user for pet information
+pet_name = input("Enter your pet's first name: ")
+pet_surname = input("Enter your pet's surname: ")
+pet_age = int(input("Enter your pet's age: "))
+
+# Print pet information
+print(f"Your pet's name is {pet_name} {pet_surname} and it's age is {pet_age}!")

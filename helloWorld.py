1998427d071dc6539c3e57f3e60cd0f8a8c2c1c3
sonyaqaderi1 <sq350@bath.ac.uk>
2024-03-09 14:50:28 +0000

    Added an input for age

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,4 @@
-print(input("Enter your name: "))
-print(input("Enter your surname: "))
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+age = int(input("Enter your age: "))
+print(f"Hi {name} {surname}! You're {age} years old!")
